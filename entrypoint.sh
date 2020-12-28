628eb8a27c81862f78e77f5d0f19bbbf2a7eb93a
nalbam <me@nalbam.com>
2020-12-28 21:40:10 +0900

    v0.2.7

--- entrypoint.sh
+++ entrypoint.sh
@@ -50,11 +50,13 @@ _release_pre() {
 }
 
 _release_id() {
-  AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
-
   URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases"
+  curl \
+    -sSL \
+    -H "${AUTH_HEADER}" \
+    ${URL} > /tmp/releases
 
-  RELEASE_ID=$(curl -sSL -H \"${AUTH_HEADER}\" ${URL} | TAG_NAME=${TAG_NAME} jq -r '.[] | select(.tag_name == env.TAG_NAME) | .id' | xargs)
+  RELEASE_ID=$(cat /tmp/releases | TAG_NAME=${TAG_NAME} jq -r '.[] | select(.tag_name == env.TAG_NAME) | .id' | xargs)
 
   echo "RELEASE_ID: ${RELEASE_ID}"
 }
